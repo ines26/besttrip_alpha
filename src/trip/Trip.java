8ef83e721e6239dcbf37fa288abb9a6cf429de45
ines <noussa6@gmail>
2015-01-15 12:16:37 +0100

    fichier trip modifié

--- src/trip/Trip.java
+++ src/trip/Trip.java
@@ -16,6 +16,7 @@ public class Trip {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello hadhoud");
     }
     
 }
